4180e6f7baf95d2b5b8fad4a0d1c1b79eac14e22
Zaza <96164075+Zaza2215@users.noreply.github.com>
2023-06-07 21:23:49 +0300

    Create model Task

--- backend/taskmanager/taskapp/models.py
+++ backend/taskmanager/taskapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=48, blank=False, null=False)
+    description = models.TextField()
+    date_until = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
