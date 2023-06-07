c0a8affea6baa1cc4ecec4dad5639e2f97531e86
Zaza <96164075+Zaza2215@users.noreply.github.com>
2023-06-07 21:39:56 +0300

    Connect to Postgresql

--- backend/taskmanager/taskmanager/settings.py
+++ backend/taskmanager/taskmanager/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = "taskmanager.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+        "PORT": os.getenv("DB_PORT", "54322"),
     }
 }
 
