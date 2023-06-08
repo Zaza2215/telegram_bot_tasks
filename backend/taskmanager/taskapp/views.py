ddb32e5c0bd9289aa5887fd77c3e19a94fef50e6
Zaza <96164075+Zaza2215@users.noreply.github.com>
2023-06-08 20:57:45 +0300

    view Create User

--- backend/taskmanager/taskapp/views.py
+++ backend/taskmanager/taskapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class UserAPI(APIView):
+    def post(self, request):
+        if not User.objects.filter(username=request.POST["username"]).exists():
+            User.objects.create_user(username=request.POST["username"], password=request.POST["password"])
+        return Response({"True"})
