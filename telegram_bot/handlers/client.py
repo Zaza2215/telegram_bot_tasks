fb0ef4b917bf7f14182a7a165a6761715dc09349
Zaza <96164075+Zaza2215@users.noreply.github.com>
2023-06-11 15:33:15 +0300

    Implement get list of tasks
    * create send_request_json
    * update HELP

--- telegram_bot/handlers/client.py
+++ telegram_bot/handlers/client.py
@@ -1,11 +1,12 @@
 from datetime import date, timedelta
+import json
 
 from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 from create_bot import dp, bot
-from requests_aiohttp import send_request
+from requests_aiohttp import send_request, send_request_json
 
 
 class FSMCreateTask(StatesGroup):
@@ -23,6 +24,7 @@ This bot helps to manage your tasks.
 /create - create task
 /edit - edit task
 /delete - delete task
+/done - mark task
 """
 
 
@@ -91,10 +93,31 @@ async def load_taskdate(message: types.Message, state: FSMContext):
     await state.finish()
 
 
+async def get_tasks(message: types.Message):
+    data = {
+        "username": message["from"]["username"],
+        "password": message["from"]["id"],
+    }
+
+    response = await send_request_json(url="http://localhost:8000/api/v1/tasks/", data=data, method="GET")
+
+    task_str = ""
+    for num, task in enumerate(response, 1):
+        if task["done"]:
+            task_str += f"☑️ {num}. "
+        else:
+            task_str += f"⭕️ {num}. "
+        task_str += task["name"].ljust(48, ".") + task["date_until"] + "\n"
+
+    await message.answer(task_str)
+    await message.delete()
+
+
 def register_handlers_client(disp: Dispatcher = dp):
     disp.register_message_handler(start_command, commands=["start"])
     disp.register_message_handler(help_command, commands=["help"])
     disp.register_message_handler(create_task, commands=["create"])
+    disp.register_message_handler(get_tasks, commands=["tasks"])
     disp.register_message_handler(load_taskname, state=FSMCreateTask.name)
     disp.register_message_handler(load_taskdescription, state=FSMCreateTask.description)
     disp.register_message_handler(load_taskdate, state=FSMCreateTask.date)
--- telegram_bot/requests_aiohttp.py
+++ telegram_bot/requests_aiohttp.py
@@ -7,18 +7,34 @@ async def send_request(url, data, method: str):
 
         if method == "GET":
             async with session.get(url, data=data) as response:
-                await response.read()
                 return response
         elif method == "POST":
             async with session.post(url, data=data) as response:
                 return response
         elif method == "PUT":
             async with session.put(url, data=data) as response:
-                await response.read()
                 return response
         elif method == "DELETE":
             async with session.delete(url, data=data) as response:
-                await response.read()
                 return response
+        else:
+            return "This method is not implemented"
+
+
+async def send_request_json(url, data, method: str):
+    async with aiohttp.ClientSession() as session:
+
+        if method == "GET":
+            async with session.get(url, data=data) as response:
+                return await response.json()
+        elif method == "POST":
+            async with session.post(url, data=data) as response:
+                return await response.json()
+        elif method == "PUT":
+            async with session.put(url, data=data) as response:
+                return await response.json()
+        elif method == "DELETE":
+            async with session.delete(url, data=data) as response:
+                return await response.json()
         else:
             return "This method is not implemented"
